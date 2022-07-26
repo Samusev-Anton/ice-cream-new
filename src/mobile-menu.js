26edfecb0bfa7232d02825194107cc370db030f2
MishaTkachev <99650019+MishaTkachev@users.noreply.github.com>
2022-07-26 03:21:01 +0300

    fixed

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -12,4 +12,11 @@
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
+  // Закрываем мобильное меню на более широких экранах.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    body.classList.remove('noscroll');
+  });
 })();
