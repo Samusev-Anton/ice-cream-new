70934e292646ef6e0f76cc00f090006dc815b50d
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-07-27 21:13:48 +0300

    close menu after click

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close'),
     menu: document.querySelector('.site-nav-wrap'),
+    mobMenu: document.querySelector('.header-nav'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.mobMenu.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
