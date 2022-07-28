bc96598df4dfb14e5d7098fe5081460d6000c94f
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-07-28 08:37:58 +0300

    Update mobile-menu.js

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -12,13 +12,13 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    // refs.body.classList.toggle('no-scroll');
   }
   // Закрываем мобильное меню на более широких экранах.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    body.classList.remove('no-scroll');
+    // body.classList.remove('no-scroll');
   });
 })();
